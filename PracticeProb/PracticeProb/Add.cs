c766cb6291a96a46dc02ab0b9a137f38fd587241
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-01-13 12:20:33 +0530

    [Raja]:Added addition of two number implemented in Demopractice

---
+++ PracticeProb/PracticeProb/Add.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class Add
+    {
+        public static void AddTwoNumbers()
+        {
+            // Adding of two numbers by taking the input from user
+            Console.WriteLine("Enter a First Number :");
+            int firstNumber=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a Second Number :");
+            int secondNumber=Convert.ToInt32(Console.ReadLine());
+            int result=firstNumber + secondNumber;
+            Console.WriteLine("adding of two numbers = " + result);
+        }
+    }
+}
