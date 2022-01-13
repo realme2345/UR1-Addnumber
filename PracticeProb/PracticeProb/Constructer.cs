9875a8a510d3c2d9c3b05ee15919cf9e644148be
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-01-13 13:52:43 +0530

    [Raja]:Added constructor program implemented in Demopractice

---
+++ PracticeProb/PracticeProb/Constructer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class Constructer
+    {
+         public int num;
+         public string Name;
+
+         public Constructer(int Num,string name)// create a constructor with number of parameters
+        {
+            num = Num; // Assigning the user values 
+            Name = name;
+            Console.WriteLine(Num+"  "+Name);// It will give the Result
+
+        }
+    }
+}
